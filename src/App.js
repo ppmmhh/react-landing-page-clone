1b39b7df165b25ee52093cd61500fd8d67805812
“Pia <pia.hoelzl@gmx.at>
2024-01-23 21:35:40 +0100

    Insert Landin Page Elements

--- src/App.js
+++ src/App.js
@@ -1,36 +1,86 @@
-import './App.css';
+import styles from './App.module.scss';
 
 export default function App() {
   return (
-  <div>
-    <header>
+    <div className={styles.app}>
+      <header className={styles.header}>
+        <img src="/images/Ellement_logo.png" alt="logo" />
 
-      <div className='Logo'/>
+        <nav>
+          <ul>
+            <li>
+              <a className={styles.link} href="/">
+                Welcome
+              </a>
+            </li>
+            <li>
+              <a className={styles.link} href="/">
+                Shop
+              </a>
+            </li>
+            <li>
+              <a className={styles.link} href="/">
+                Build Your Box
+              </a>
+            </li>
+            <li>
+              <a className={styles.link} href="/">
+                By Condition
+              </a>
+            </li>
+            <li>
+              <a className={styles.link} href="/">
+                Our Quality
+              </a>
+            </li>
+            <li>
+              <a className={styles.link} href="/">
+                Journal
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
 
-      <nav>
-        <ul>
-          <li>
-            <a className={styles.link} href="/">Welcome</a>
-          </li>
-          <li>
-            <a className={styles.link} href="/">Shop</a>
-          </li>
-          <li>
-            <a className={styles.link} href="/">Build Your Box</a>
-          </li>
-          <li>
-            <a className={styles.link} href="/">By Condition</a>
-          </li>
-          <li>
-            <a className={styles.link} href="/">Our Quality</a>
-          </li>
-          <li>
-            <a className={styles.link} href="/">Journal</a>
-          </li>
-        </ul>
-      </nav>
+      <main>
+        <section>
+          <h1>Prenatals made just for you.</h1>
+          <p>
+            {' '}
+            Welcome to Ellement, the world’s first personalized prenatal
+            supplement. Packed fresh each month for your unique needs during
+            every stage of your journey—from preconception to postpartum.
+          </p>
+          <div className={styles.buttons}>
+              <Button Box>Build Your Box</Button>
+        </section>
 
-    </header>
-  </div>
+        <section>
+          <p>AS FEATURED BY</p>
+// ADD LOGOS
+        </section>
 
+        <section>
+// ADD IMG
+          <h1>Every body is unique. Your supplements should be, too.
+          </h1>
+          <p>Ellement takes all the guesswork out of prenatal vitamins. Every month, you receive custom-made daily packets that are personalized for your specific nutritional needs and context, simplifying your prenatal supplement routine.
+        </p>
+        <div className={styles.button}>
+              <Button Box>Build Your Box</Button>
+        </section>
+
+        <section>
+// RUNNING BANNER
+        </section>
+
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+      </main>
+
+      <footer></footer>
+    </div>
+  );
 }
